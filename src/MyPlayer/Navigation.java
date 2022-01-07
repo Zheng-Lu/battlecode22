6f28b2475b5487d94a20ddcc228a393ac1f7aaff
Henry8772 <zhonghe.zhang@hotmail.com>
2022-01-07 20:28:00 +0000

    fixed bug in moveTo

--- src/MyPlayer/Navigation.java
+++ src/MyPlayer/Navigation.java
@@ -1,7 +1,8 @@
 package MyPlayer;
 
 import battlecode.common.*;
-import java.util.PriorityQueue;
+import java.util.*;
+
 
 interface NavSafetyPolicy {
     public boolean isSafeToMoveTo(MapLocation loc) throws GameActionException;
@@ -21,10 +22,39 @@ class SafetyPolicyCrunch extends Bot implements NavSafetyPolicy {
     }
 }
 
+
+
+
+
 public class Navigation extends Bot {
 
     public Navigation(RobotController r) throws GameActionException {
         super(r);
+    }
+
+    static class Tuple implements Comparable<Tuple>{
+        MapLocation loc;
+        Integer priority;
+         
+        public Tuple(MapLocation loc, Integer priority){
+            this.loc = loc;
+            this.priority = priority;
+        }
+
+        public MapLocation getLoc(){
+            return this.loc;
+        }
+
+        // public Integer getPriority(){
+        //     return this.priority;
+        // }
+ 
+        @Override
+        public int compareTo(Tuple tup) {
+            return this.priority - tup.priority;
+        }
+         
+         
     }
 
     // TODO: Move to specific given destination
@@ -40,46 +70,47 @@ public class Navigation extends Bot {
             Direction.NORTHWEST,
         };
         MapLocation curLoc = rc.getLocation();
+        
         if(!rc.isMovementReady()){
             // Actions needed when rc can't move
         }
         else if(curLoc.isAdjacentTo(destination)){
-            rc.move(curloc.directionTo(destination));
+            rc.move(curLoc.directionTo(destination));
         }
         else{
-        //Minor bug
-        //     PriorityQueue<MapLocation, Integer> pQueue = new PriorityQueue<>();
-        //     pQueue.add(curLoc, rc.senseRubble(curLoc));
-        //     Hashtable<MapLocation, Integer> visited = new Hashtable<MapLocation, Integer>();
-        //     Hashtable<MapLocation, MapLocation> path = new Hashtable<>();
-        //     MapLocation[] allDirections = new MapLocation;
-        //     //Store all the directions needed to reach the destination
-        //     visited.put(curLoc, rc.senseRubble(curLoc));
-        //     path.put(null, curLoc);
+            PriorityQueue<Tuple> pQueue = new PriorityQueue<Tuple>();
 
-        //     while (!pQueue.isEmpty()){
-        //         MapLocation current = pQueue.poll();
+            pQueue.add(new Tuple(curLoc, rc.senseRubble(curLoc)));
+            Hashtable<MapLocation, Integer> visited = new Hashtable<MapLocation, Integer>();
+            Hashtable<MapLocation, MapLocation> path = new Hashtable<>();
+            ArrayList<Direction> allDirections = new ArrayList<Direction>(); 
+            //Store all the directions needed to reach the destination
+            visited.put(curLoc, rc.senseRubble(curLoc));
+            path.put(null, curLoc);
 
-        //         if(current.equals(destination)){
-        //             break;
-        //         }
+            while (!pQueue.isEmpty()){
+                MapLocation current = pQueue.poll().getLoc();
 
-        //         for(Direction direction : directions){
-        //             MapLocation adjLoc = current.add(direction);
-        //             Integer new_cost = rc.senseRubble(adjLoc) + visited.get(current);
-        //             if(!visited.containsKey(adjLoc)){
-        //                 visited.put(adjLoc, new_cost);    
-        //                 Integer priority = new_cost + adjLoc.distanceSquaredTo(destination);
-        //                 pQueue.put(adjLoc, priority);
-        //                 path.put(current, adjLoc);
-        //                 allDirections.add(direction);
-        //             }
-        //         }
+                if(current.equals(destination)){
+                    break;
+                }
+
+                for(Direction direction : directions){
+                    MapLocation adjLoc = current.add(direction);
+                    Integer new_cost = rc.senseRubble(adjLoc) + visited.get(current);
+                    if(!visited.containsKey(adjLoc)){
+                        visited.put(adjLoc, new_cost);    
+                        Integer priority = new_cost + adjLoc.distanceSquaredTo(destination);
+                        pQueue.add(new Tuple(adjLoc, priority));
+                        path.put(current, adjLoc);
+                        allDirections.add(direction);
+                    }
+                }
             
-        //     }
+            }
 
-        //     rc.move(allDirections[1]);
-        // }
+            rc.move(allDirections.get(1));
+        }
             
 
 

24b6ff3c93a9c324ad43fcc5ead9c82a14f1d66d
Zheng-Lu <s2085615@ed.ac.uk>
2022-01-14 16:51:05 +0000

    lubao_2 update

---
+++ src/lubao_2/LaboratoryStrategy.java
@@ -0,0 +1,11 @@
+package lubao_2;
+
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+
+
+strictfp class LaboratoryStrategy {
+    static void run(RobotController rc) throws GameActionException {
+        
+    }
+}
